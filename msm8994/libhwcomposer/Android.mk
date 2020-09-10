17e178ab6d5dd8382efcc76a490266fb54af5c3a
YumeMichi <do4suki@gmail.com>
2020-09-10 15:55:16 +0800

    msm8994: hwcomposer: Disable Dynamic FPS
     * The build flag has been replaced with system prop.
    
    Signed-off-by: YumeMichi <do4suki@gmail.com>
    Change-Id: I36c22d209d92c68797f78e27d7a91b1ce45a0307

--- msm8994/libhwcomposer/Android.mk
+++ msm8994/libhwcomposer/Android.mk
@@ -21,11 +21,6 @@ endif #TARGET_USES_QCOM_BSP
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdhwcomposer\" \
                                  -Wno-sign-conversion -Wno-float-conversion -Wno-sizeof-array-argument -Wno-pointer-bool-conversion
 
-#Enable Dynamic FPS if PHASE_OFFSET is not set
-ifeq ($(VSYNC_EVENT_PHASE_OFFSET_NS),)
-    LOCAL_CFLAGS += -DDYNAMIC_FPS
-endif
-
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 LOCAL_SRC_FILES               := hwc.cpp          \
                                  hwc_utils.cpp    \
